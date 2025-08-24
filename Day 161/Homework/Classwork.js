c8dc372bd264de0814eeb4db9055e552b6e76e50
GuramiPapunashvili <gurampapunashvili@mziuri.ge>
2025-08-24 21:49:25 +0400

    commit

---
+++ Day 161/Homework/Classwork.js
@@ -0,0 +1,37 @@
+import fs from 'fs'
+
+// process.stdin.on('data', (data) => {
+//     const password = data.toString();
+//     console.log('X'.repeat(password.length));
+//     process.exit()
+// })
+
+//Methods
+//Post -- Post method is when the we/user sends information to the server
+//Get -- we/user Reciving the information from the server
+//Put - Changing the information on the server
+//Delete -- Deleting the information from the server
+
+
+//Path
+//https://example.com/testplace?by-kwty23
+//https - communication protocol
+//domain(example.com) - domain(simple face of IP)
+//path(testplace?) - location of our file in the domain
+
+
+//HTTP versions
+//We have 3 versions of HTTP: 1.1, 2 and 3. each of them have their advantages, all 3 are usable today
+//1.1 - 
+//2 - 
+//3 - 
+
+//Headers - additional information , optional to send
+//Body - also optional to send
+
+
+
+
+
+//Status codes
+//Status codes are just numbers that mean certain things, if there's an error instead of receiving a huge paragraph you receive a status code, the code could mean succes or an error of such
